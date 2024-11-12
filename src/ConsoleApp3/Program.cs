8198c593592197ceaec4a6850cb8309cdd601a20
AUCODE12 <qahmadjon11@gmail.com>
2024-11-12 09:00:19 +0500

    update

---
+++ src/ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter text: ");
+            var text = Console.ReadLine();
+
+            Console.Write(withoutX(text));
+        }
+        public static string withoutX(string text)
+        {
+            if (text.StartsWith('x') is true)
+            {
+                return text.Substring(1);
+            }
+            else if (text.EndsWith('x') is true)
+            {
+                return text.Substring(0, text.Length - 1);
+            }
+            else
+            {
+                return text;
+            }
+        }
+    }
+}
